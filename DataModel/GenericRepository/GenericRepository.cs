f411e5f9a1d61e6fd5600cb65f6b4de07574935d
akspatil <akshatha.patil.22@gmail.com>
2017-06-21 20:16:15 +0530

    Repository and Unit of work Code 21/06/17

--- DataModel/GenericRepository/GenericRepository.cs
+++ DataModel/GenericRepository/GenericRepository.cs
@@ -1,12 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataModel.GenericRepository
 {
-    class GenericRepository
+    public class GenericRepository<TEntity> where TEntity : class
     {
+        internal INSTITUTERECORDEntities Context;
+        internal DbSet<TEntity> DbSet;
+
+        public GenericRepository(INSTITUTERECORDEntities context)
+        {
+            this.Context = context;
+            this.DbSet = context.Set<TEntity>();
+        }
+
+        public virtual IEnumerable<TEntity> Get()
+        {
+            IQueryable<TEntity> query = DbSet;
+            return query.ToList();
+        }
+
+        public virtual TEntity GetById(object id)
+        {
+            return DbSet.Find(id);
+        }
+
+        public virtual void Insert(TEntity entity)
+        {
+            DbSet.Add(entity);
+        }
+
+        public virtual void Delete(object id)
+        {
+            TEntity entity = DbSet.Find(id);
+            Delete(entity);
+        }
+
+        public virtual void Delete (TEntity entityToDelete)
+        {
+            if (Context.Entry(entityToDelete).State == EntityState.Detached)
+            {
+                DbSet.Attach(entityToDelete);
+            }
+            DbSet.Remove(entityToDelete);
+        }
+
+        public virtual void Update(TEntity entityToUpdate)
+        {
+            DbSet.Attach(entityToUpdate);
+            Context.Entry(entityToUpdate).State = EntityState.Modified;
+        }
+
+        public virtual IEnumerable<TEntity> GetMany(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).ToList();
+        }
+
+        public virtual IQueryable<TEntity> GetManyQueryable(Func<TEntity, bool> where)
+        {
+            return DbSet.Where(where).AsQueryable();
+        }
+
+        public TEntity Get(Func<TEntity, Boolean> where)
+        {
+            return DbSet.Where(where).FirstOrDefault();
+        }
+
+        public void Delete(Func<TEntity, bool> where)
+        {
+            IQueryable<TEntity> entities = DbSet.Where(where).AsQueryable();
+            foreach (var entity in entities)
+            {
+                DbSet.Remove(entity);
+            }
+        }
+
+        public virtual IEnumerable<TEntity> GetAll()
+        {
+            return DbSet.ToList();
+        }
+
+        public bool Exists(object primaryKey)
+        {
+            return DbSet.Find(primaryKey) != null;
+        }
+
+        public TEntity GetSingle(Func<TEntity, bool> predicate)
+        {
+            return DbSet.Single<TEntity>();
+        }
+
+        public TEntity GetFirst(Func<TEntity, bool> entity)
+        {
+            return DbSet.First<TEntity>();
+        }
     }
 }
---
+++ DataModel/UnitofWork/UnitOfWork.cs
@@ -0,0 +1,112 @@
+﻿using DataModel.GenericRepository;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModel.UnitofWork
+{
+    public class UnitOfWork : IDisposable
+    {
+        private INSTITUTERECORDEntities _context = null;
+        private GenericRepository<User> _userRepository;
+        private GenericRepository<Token> _tokenRepository;
+        private GenericRepository<StudentDetail> _studentRepository;
+        private GenericRepository<Payment> _paymentRepository;
+
+        public UnitOfWork()
+        {
+            _context = new INSTITUTERECORDEntities();
+        }
+
+        public GenericRepository<User> UserRepository
+        {
+            get
+            {
+                if (this._userRepository == null)
+                    this._userRepository = new GenericRepository<User>(_context);
+                return _userRepository;
+            }
+        }
+
+        public GenericRepository<Token> TokenRepository
+        {
+            get
+            {
+                if (this._tokenRepository == null)
+                    this._tokenRepository = new GenericRepository<Token>(_context);
+                return _tokenRepository;
+            }
+        }
+
+        public GenericRepository<StudentDetail> StudentRepository
+        {
+            get
+            {
+                if (this._studentRepository == null)
+                    this._studentRepository = new GenericRepository<StudentDetail>(_context);
+                return _studentRepository;
+            }
+        }
+
+        public GenericRepository<Payment> PaymentRepository
+        {
+            get
+            {
+                if (this._paymentRepository == null)
+                    this._paymentRepository = new GenericRepository<Payment>(_context);
+                return _paymentRepository;
+            }
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbEntityValidationException e)
+            {
+
+                var outputLines = new List<string>();
+                foreach (var eve in e.EntityValidationErrors)
+                {
+                    outputLines.Add(string.Format(
+                        "{0}: Entity of type \"{1}\" in state \"{2}\" has the following validation errors:", DateTime.Now, 
+                        eve.Entry.Entity.GetType().Name, eve.Entry.State));
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        outputLines.Add(string.Format("- Property: \"{0}\", Error: \"{1}\"", ve.PropertyName, ve.ErrorMessage));
+                    }
+                }
+                System.IO.File.AppendAllLines(@"C:\errors.txt", outputLines);
+
+                throw e;
+            }
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    Debug.WriteLine("Unit of work is disposing");
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
